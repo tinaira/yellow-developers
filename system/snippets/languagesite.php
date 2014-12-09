d3032e2d994e4b6cdf6ae1e59d24e5909a273e6b
markseu <mark2011@mayberg.se>
2014-12-09 11:28:51 +0100

    Demo cleanup

--- system/snippets/languagesite.php
+++
@@ -1,7 +0,0 @@
-<div class="languagesite">
-<ul>
-<?php foreach($yellow->pages->translation("/") as $page): ?>
-<li><a href="<?php echo $page->getLocation() ?>"><?php echo $yellow->text->getTextHtml("languageDescription", $page->get("language")) ?></a></li>
-<?php endforeach ?>
-</ul>
-</div>
