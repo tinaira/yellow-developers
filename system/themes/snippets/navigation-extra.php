abc3c66881c0d914545e6bcdf5c5aa6a349a362a
markseu <mark2011@mayberg.se>
2015-05-24 17:29:57 +0200

    Hello demo (core update)

--- system/themes/snippets/navigation-extra.php
+++ system/themes/snippets/navigation-extra.php
@@ -5,6 +5,7 @@
 <?php foreach($pages as $page): ?>
 <li><a<?php echo $page->isActive() ? " class=\"active\"" : "" ?> href="<?php echo $page->getLocation() ?>"><?php echo $page->getHtml("titleNavigation") ?></a></li>
 <?php endforeach ?>
+<li><a href="<?php echo $yellow->page->base."/search/" ?>"><i class="fa fa-search"></i></a></li>
 </ul>
 </div>
 <div class="navigation-banner"></div>
