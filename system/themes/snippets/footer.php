2f6a262be7e16dedb203531d5a2118ad8f219540
markseu <mark2011@mayberg.se>
2016-04-12 15:58:32 +0200

    Developers kit update

--- system/themes/snippets/footer.php
+++ system/themes/snippets/footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
 <div class="footer-left">
-<a href="http://datenstrom.se/yellow">Made with Yellow</a>.
+<a href="<?php echo $yellow->text->get("yellowUrl") ?>">Made with Yellow</a>.
 <a href="<?php echo $yellow->page->base."/help/status" ?>">Status</a>.
 <a href="<?php echo $yellow->page->get("pageEdit") ?>">Edit</a>.
 </div>
@@ -9,8 +9,8 @@
 <a href="https://github.com/datenstrom"><i class="fa fa-github" title="GitHub"></i></a>
 <a href="https://gitter.im/datenstrom/yellow"><i class="fa fa-comment" title="Gitter"></i></a>
 </div>
-</div>
 <div class="footer-banner"></div>
+</div>
 </div>
 <?php echo $yellow->page->getExtra("footer") ?>
 </body>
