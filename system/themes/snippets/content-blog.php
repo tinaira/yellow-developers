51019577c8b3f49a969ba06c36bf7d3c72e96bf7
markseu <mark2011@mayberg.se>
2016-09-04 10:12:59 +0200

    Developers kit update

--- system/themes/snippets/content-blog.php
+++ system/themes/snippets/content-blog.php
@@ -5,7 +5,7 @@
 <?php endif ?>
 <div class="<?php echo $yellow->page->getHtml("entryClass") ?>">
 <div class="entry-header"><h1><?php echo $yellow->page->getHtml("titleContent") ?></h1></div>
-<div class="entry-meta"><?php echo htmlspecialchars($yellow->page->getDate("published")) ?> <?php echo $yellow->text->getHtml("blogBy") ?> <?php $authorCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("author")) as $author) { if(++$authorCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("author:$author")."\">".htmlspecialchars($author)."</a>"; } ?></div>
+<div class="entry-meta"><?php echo $yellow->page->getDateHtml("published") ?> <?php echo $yellow->text->getHtml("blogBy") ?> <?php $authorCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("author")) as $author) { if(++$authorCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("author:$author")."\">".htmlspecialchars($author)."</a>"; } ?></div>
 <div class="entry-content"><?php echo $yellow->page->getContent() ?></div>
 <div class="entry-footer">
 <?php if($yellow->page->isExisting("tag")): ?>
