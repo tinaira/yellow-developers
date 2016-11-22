8a4210b5c676eefb2640ae77db280cbc709cc8dc
markseu <mark2011@mayberg.se>
2016-11-22 15:25:57 +0100

    Developers kit update

--- system/themes/snippets/content-blog.php
+++ system/themes/snippets/content-blog.php
@@ -14,7 +14,8 @@
 <p><?php echo $yellow->text->getHtml("blogTag") ?> <?php $tagCounter = 0; foreach(preg_split("/,\s*/", $yellow->page->get("tag")) as $tag) { if(++$tagCounter>1) echo ", "; echo "<a href=\"".$yellow->page->getPage("blog")->getLocation(true).$yellow->toolbox->normaliseArgs("tag:$tag")."\">".htmlspecialchars($tag)."</a>"; } ?></p>
 <?php endif ?>
 </div>
-</div>
+<?php echo $yellow->page->getExtra("links") ?>
 <?php echo $yellow->page->getExtra("comments") ?>
+</div>
 </div>
 </div>
