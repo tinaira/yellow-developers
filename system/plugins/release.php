c8e6f7b3447b4e6558dedcabce4650453a9f69df
markseu <mark2011@mayberg.se>
2017-02-18 16:06:21 +0100

    Developers kit update

--- system/plugins/release.php
+++ system/plugins/release.php
@@ -1,5 +1,5 @@
 <?php
-// Release plugin, https://github.com/datenstrom/yellow-plugins/tree/master/release
+// Release plugin
 // Copyright (c) 2013-2017 Datenstrom, https://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
 
