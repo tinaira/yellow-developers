51d83de4aca7bcbf8e4cd7305fd29eccb5eba109
markseu <mark2011@mayberg.se>
2017-06-26 12:54:29 +0200

    Developer kit update

--- system/themes/snippets/footer-custom.php
+++ system/themes/snippets/footer-custom.php
@@ -6,8 +6,8 @@
 <a href="<?php echo $yellow->page->get("pageEdit") ?>">Edit</a> &nbsp;
 </div>
 <div class="siteinfo-right">
-<a href="<?php echo $yellow->page->base."/help/support" ?>">Support</a> &nbsp;
 <a href="<?php echo $yellow->page->base."/help/api" ?>">API</a> &nbsp;
+<a href="<?php echo $yellow->page->base."/help/support" ?>">Support</a> &nbsp;
 <a href="<?php echo $yellow->text->get("yellowUrl") ?>">Made with Yellow</a>
 </div>
 <div class="siteinfo-banner"></div>
