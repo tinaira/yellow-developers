465f9cc394f55fef8ded80e6fdfa47b4430ef734
markseu <mark2011@mayberg.se>
2018-02-26 10:23:15 +0100

    Developer kit update

--- system/themes/snippets/content-search.php
+++ system/themes/snippets/content-search.php
@@ -4,7 +4,7 @@
 <h1><?php echo $yellow->page->getHtml("titleSearch") ?></h1>
 <?php if($yellow->page->get("navigation")!="navigation-search"): ?>
 <form class="search-form" action="<?php echo $yellow->page->getLocation(true) ?>" method="post">
-<input class="form-control" type="text" name="query" value="<?php echo htmlspecialchars($_REQUEST["query"]) ?>" />
+<input class="form-control" type="text" name="query" value="<?php echo htmlspecialchars($_REQUEST["query"]) ?>"<?php echo $yellow->page->get("status")=="none" ? " autofocus=\"autofocus\"" : "" ?> />
 <input class="btn search-btn" type="submit" value="<?php echo $yellow->text->getHtml("searchButton") ?>" />
 <input type="hidden" name="clean-url" />
 </form>
@@ -17,7 +17,7 @@
 <div class="entry-location"><a href="<?php echo $page->getLocation(true) ?>"><?php echo $page->getUrl() ?></a></div>
 </div>
 <?php endforeach ?>
-<?php else: ?>
+<?php elseif($yellow->page->get("status")!="none"): ?>
 <p><?php echo $yellow->text->getHtml("searchResults".ucfirst($yellow->page->get("status"))) ?><p>
 <?php endif ?>
 <?php $yellow->snippet("pagination", $yellow->page->getPages()) ?>
