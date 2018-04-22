f9f9c611b14e95c659cfc7542c43f91a03785cb8
markseu <mark2011@mayberg.se>
2018-04-22 19:57:53 +0200

    Developer kit update, fixed broken links

--- system/plugins/disqus.php
+++ system/plugins/disqus.php
@@ -1,11 +1,11 @@
 <?php
 // Disqus plugin, https://github.com/datenstrom/yellow-plugins/tree/master/disqus
-// Copyright (c) 2013-2017 Datenstrom, https://datenstrom.se
+// Copyright (c) 2013-2018 Datenstrom, https://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
 
 class YellowDisqus
 {
-	const VERSION = "0.7.2";
+	const VERSION = "0.7.3";
 	var $yellow;			//access to API
 	
 	// Handle initialisation
@@ -31,11 +31,11 @@ class YellowDisqus
 			$output .= "var disqus_config = function () { this.language = '".strencode($language)."'; };\n";
 			$output .= "(function() {\n";
 			$output .= "var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\n";
-			$output .= "dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\n";
+			$output .= "dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';\n";
 			$output .= "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\n";
 			$output .= "})();\n";
 			$output .= "</script>\n";
-			$output .= "<noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\n";
+			$output .= "<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\n";
 		}
 		return $output;
 	}
