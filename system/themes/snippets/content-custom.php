37c7f7c30d689851824ab69e33721850ddfa61d7
markseu <mark2011@mayberg.se>
2018-04-26 17:49:13 +0200

    Developer kit update, API

--- system/themes/snippets/content-custom.php
+++ system/themes/snippets/content-custom.php
@@ -5,8 +5,14 @@
 <div class="main">
 <h1><?php echo $yellow->page->getHtml("titleContent") ?></h1>
 <?php echo $yellow->page->getContent() ?>
+<table>
 <?php foreach($pages as $page): ?>
-<h3><a href="<?php echo $page->getLocation(true) ?>"><?php echo $page->getDateHtml("published") ?> &nbsp; <?php echo $page->getHtml("title") ?></a></h3>
+<tr>
+<td><a href="<?php echo $page->getLocation(true) ?>"><?php echo $page->getHtml("title") ?></a></td>
+<td><?php echo $page->getHtml("author") ?></td>
+<td><?php echo $page->getDateRelativeHtml("published") ?></td>
+</tr>
 <?php endforeach ?>
+</table>
 </div>
 </div>
