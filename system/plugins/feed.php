9989938d1039eed3dd6d0425ae747d3843670d72
markseu <mark2011@mayberg.se>
2018-06-16 22:13:13 +0200

    Developer kit update

--- system/plugins/feed.php
+++ system/plugins/feed.php
@@ -5,7 +5,7 @@
 
 class YellowFeed
 {
-	const VERSION = "0.7.3";
+	const VERSION = "0.7.4";
 	var $yellow;			//access to API
 	
 	// Handle initialisation
@@ -42,13 +42,13 @@ class YellowFeed
 			{
 				$pages->sort($chronologicalOrder ? "modified" : "published", false);
 				$pages->limit($this->yellow->config->get("feedPaginationLimit"));
-				$title = !empty($pagesFilter) ? implode(' ', $pagesFilter)." - ".$this->yellow->page->getHtml("sitename") : $this->yellow->page->getHtml("sitename");
+				$title = !empty($pagesFilter) ? implode(' ', $pagesFilter)." - ".$this->yellow->page->get("sitename") : $this->yellow->page->get("sitename");
 				$this->yellow->page->setLastModified($pages->getModified());
 				$this->yellow->page->setHeader("Content-Type", "application/rss+xml; charset=utf-8");
 				$output = "<?xml version=\"1.0\" encoding=\"utf-8\"\077>\r\n";
 				$output .= "<rss version=\"2.0\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\r\n";
 				$output .= "<channel>\r\n";
-				$output .= "<title>".$title."</title>\r\n";
+				$output .= "<title>".htmlspecialchars($title)."</title>\r\n";
 				$output .= "<link>".$this->yellow->page->scheme."://".$this->yellow->page->address.$this->yellow->page->base."/"."</link>\r\n";
 				$output .= "<description>".$this->yellow->page->getHtml("tagline")."</description>\r\n";
 				$output .= "<language>".$this->yellow->page->getHtml("language")."</language>\r\n";
